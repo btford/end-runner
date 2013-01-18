a78729b31b273fa14d6293cd4f68824938b26675
Brian Ford <btford@umich.edu>
2013-01-18 18:56:22 -0500

    fix issue that clears mapCanvas

--- public/js/directives/game.js
+++ public/js/directives/game.js
@@ -4,29 +4,37 @@ angular.module('gameApp').directive('game',
     function ($window, fullscreen, gameController, dataLoader, sharedModel) {
 
   var canvasWidth = 1000,
-    canvasHeight = 600;
-
-
-  var tileSize = 60;
+    canvasHeight = 600,
+    tileSize = 60;
 
   return {
     templateUrl: '/directives/game',
     restrict: 'E',
     link: function (scope, elm, attrs) {
 
+      /*
+       * Render Map
+       */
+
       var mapCanvas = elm.find('canvas')[1];
+      var mapContext = mapCanvas.getContext('2d');
+
       mapCanvas.width = 1920;
       mapCanvas.height = 1080;
-      var mapContext = canvas.getContext('2d');
 
       var levelModel = dataLoader.get('/json/levels/level-one.json');
       
       var renderMap = function () {
         levelModel.tiles.forEach(function (col, row) {
           for (var i = 0; i < col.length; i++) {
+
             switch (col[i]) {
               case ' ':
                 mapContext.fillStyle = "#fff";
+                break;
+
+              case '=':
+                mapContext.fillStyle = "#ccc";
                 break;
 
               default:
@@ -39,12 +47,32 @@ angular.module('gameApp').directive('game',
         });
       };
 
+      renderMap();
 
+
+      /*
+       * Mock Models
+       */
+
+      var pretendModel = {
+        player1: {
+          x: 5,
+          y: 5
+        },
+        player2: {
+          x: 200,
+          y: 5
+        }
+      };
+
+
+      /*
+       * Render Models
+       */
 
       var canvas = elm.find('canvas')[0];
       var context = canvas.getContext('2d');
       
-
       canvas.width = canvasWidth;
       canvas.height = canvasHeight;
 
@@ -59,22 +87,6 @@ angular.module('gameApp').directive('game',
           canvas.height = canvasHeight;
         }
       });
-
-      angular.element(canvas).bind('click', function (ev) {
-        // on click ...
-        
-      });
-
-      var pretendModel = {
-        player1: {
-          x: 5,
-          y: 5
-        },
-        player2: {
-          x: 200,
-          y: 5
-        }
-      };
 
       var render = function () {
         context.clearRect(0, 0, canvas.width, canvas.height);
@@ -101,10 +113,21 @@ angular.module('gameApp').directive('game',
       };
 
       // start rendering the game
-
       render();
 
-      // stop rendering when out of scope
+
+      /*
+       * Bind Events
+       */
+
+      angular.element(canvas).bind('click', function (ev) {
+        // on click ...
+      });
+
+
+      /*
+       * stop rendering when out of scope
+       */
 
       scope.$on('$destroy', function () {
         render = function () {};
