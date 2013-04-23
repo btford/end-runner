ffc54a42d14394d28ca1e4566da5f742d53338b7
Brian Ford <btford@li63-143.members.linode.com>
2013-04-23 02:19:50 -0400

    1337 port

--- server.js
+++ server.js
@@ -60,6 +60,6 @@ states.init(io);
  * Start Server
  */
 
-server.listen(3000, function(){
+server.listen(1337, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
